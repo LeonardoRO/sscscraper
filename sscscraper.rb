200058a5299fb165742f4b96fda4785b41aaa667
Leonardo Ribeiro Oliveira <LeonardoRibeiro.Oliveira@king.com>
2014-07-17 13:12:57 +0200

    added support for multiple pages

--- sscscraper.rb
+++ sscscraper.rb
@@ -2,13 +2,14 @@ require "open-uri"
 require "pathname"
 
 unless ARGV[0]
-  puts "Use ruby sscscraper.rb <url to skyscrapercity forum page>"
+  puts "Use ruby sscscraper.rb <url to page> <number of pages>"
   exit
 end
 
 url = ARGV[0].strip
+total_pages = ARGV[1].strip
 
-begin
+def get_pics(url)
   open(url, "User-Agent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36") do |source|
     source.each_line do |x|
       if x !~ /skyscrapercity.com/i && x !~ /googlesyndication.com/ && x !~ /doubleclick.net/ && x !~ /big-boards.com/
@@ -38,4 +39,11 @@ begin
     end
     end
   end
+end
+
+start_page = 1
+while start_page <= total_pages.to_i
+  url = url + "&page=#{start_page}"
+  get_pics(url)
+  start_page = start_page + 1
 end
